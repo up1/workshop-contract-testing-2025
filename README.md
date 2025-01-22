9a7b13e37c49cf58c6c04a4170cc9bbe29f3f5b6
up1 <somkiat.p@gmail.com>
2025-01-22 22:20:15 +0700

    Create provider

---
+++ README.md
@@ -0,0 +1,21 @@
+# Workshop :: Contract testing with Pact
+
+## 1. Provider
+* Movie service
+  * List of movies
+  * Get movies by id
+
+```
+$npm install
+$npm run start:provider
+```
+
+List of URLs of provider
+* API document with Swagger
+  * http://localhost:3000/swagger/
+* Get all movies
+  * http://localhost:3000/movies/
+* Get movie by id
+  * http://localhost:3000/movie/1
+
+## 2. Consumer web
---
+++ provider/provider.js
@@ -0,0 +1,167 @@
+const express = require('express');
+const cors = require('cors');
+const Movies = require('./movies');
+const Joi = require('joi');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+var corsOptions = {
+  origin: 'http://localhost:8000',
+  optionsSuccessStatus: 200
+}
+
+const server = express();
+server.use(cors(corsOptions));
+server.use(express.json());
+
+const movies = new Movies();
+
+// Load default data into the Movies class
+const importData = () => {
+  const data = require('.././data/movies.json');
+  data.reduce((a, v) => {
+    v.id = a + 1;
+    movies.insertMovie(v);
+    return a + 1;
+  }, 0);
+};
+
+/**
+ * @openapi
+ * /movies:
+ *  get:
+ *    description: Get all movies
+ *    responses:
+ *      '200':
+ *        description: A list of movie objects
+ *        content:
+ *          application/json; charset=utf-8:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: integer
+ *                  name:
+ *                    type: string
+ *                  year:
+ *                    type: integer
+ *                example:
+ *                  id: 1
+ *                  name: James Bond
+ *                  year: 2021
+ */
+server.get('/movies', (req, res) => {
+  res.send(movies.getMovies());
+});
+
+/**
+ * @openapi
+ * /movie/{id}:
+ *  get:
+ *    description: Get a movie by Id
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *          example: 1
+ *        required: true
+ *        example: 1
+ *    responses:
+ *      '200':
+ *        description: A movie object
+ *        content:
+ *          application/json; charset=utf-8:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                  type: integer
+ *                name:
+ *                  type: string
+ *                year:
+ *                  type: integer
+ *              example:
+ *                id: 1
+ *                name: James Bond
+ *                year: 2021
+ */
+server.get('/movie/:id', (req, res) => {
+  const movie = movies.getMovieById(req.params.id);
+  if (!movie) {
+    res.status(404).send({ error: 'Movie not found' });
+  } else {
+    res.send(movie);
+  }
+});
+
+server.post('/movies', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    year: Joi.number().integer().min(1900).max(2023).required(),
+  });
+
+  const result = schema.validate(req.body);
+  const id = movies.length < 1 ? (movies[movies.length - 1].id) + 1 : 1;
+
+  const movie = {
+    id,
+    name: req.body.name,
+    year: req.body.year,
+  };
+
+  if (result.error) res.status(404).send(result.error.details[0]);
+
+  if (movies.getMovieByName(req.body.name)) {
+    res.status(409).send({ error: `Movie ${req.body.name} already exists` });
+  } else {
+    movies.insertMovie(movie);
+    res.json(movie);
+  }
+});
+
+server.delete('/movie/:id', (req, res) => {
+  const movie = movies.getMovieById(req.params.id);
+  if (!movie) {
+    res.status(404).send({ error: `Movie ${req.params.id} not found` });
+  } else {
+    movies.getMovies().filter(movie => movie.id !== req.params.id)
+    res.send({ message: `Movie ${req.params.id} has been deleted` });
+  }
+});
+
+const options = {
+  failOnErrors: true,
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Provider Movies API",
+      version: "0.0.1"
+    },
+    externalDocs: {
+      description: "swagger.json",
+      url: "/swagger.json"
+    }
+  },
+  apis: ["./provider/provider.js"]
+};
+
+const specs = swaggerJsdoc(options);
+
+server.use(
+  "/swagger",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+server.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(specs);
+});
+
+module.exports = {
+  server,
+  importData,
+  movies,
+};
