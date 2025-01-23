1cff579f5e2526a5b4efb7f5703271406c8fd0ab
up1 <somkiat.p@gmail.com>
2025-01-24 00:28:18 +0700

    Contract for ui with cypress

--- cypress/e2e/movies.contract.cy.js
+++ cypress/e2e/movies.contract.cy.js
@@ -3,13 +3,14 @@ describe("Movies", () => {
     cy.setupPact("UIConsumer", "MoviesAPI");
     cy.intercept("http://localhost:3000/movie/1", {
       statusCode: 200,
-      body: { id: 1, name: "Finding nemo", year: 2003 },
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: { id: 1, name: "The Shawshank Redemption", year: 1994 },
     }).as("movies");
   });
 
   it("Movies list", () => {
     cy.visit("/");
     cy.usePactWait("movies").its("response.statusCode").should("eq", 200);
-    cy.contains("Finding nemo").should("be.visible");
+    cy.contains("The Shawshank Redemption").should("be.visible");
   });
 });
